5e48be3df6ea20d510b5d53a23846d4fa6753eea
Aleem <meliodaslord280@gmail.com>
2025-05-08 00:17:18 +0100

    updated functionalities
    Added edit,delete, auto-focus,enter key

--- src/components/TodoInputs.jsx
+++ src/components/TodoInputs.jsx
@@ -1,15 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
-const TodoInputs = ({ addTodo }) => {
+const TodoInputs = ({ addTodo, edit, updateTodo }) => {
   const [input, setInput] = useState("");
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (edit.id !== null) {
+      setInput(edit.text);
+    }
+  }, [edit]);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [edit]);
 
   const handleChange = (e) => {
     setInput(e.target.value);
   };
 
+  const handleKey = (e) => {
+    if (e.key === "ENTER") {
+      handleSubmit(e);
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    addTodo(input);
+
+    if (!input.trim()) return;
+
+    if (edit.id !== null) {
+      updateTodo(edit.id, input);
+    } else {
+      const newTodo = { id: Math.floor(Math.random() * 1000), text: input };
+
+      addTodo(newTodo);
+    }
+
     setInput("");
   };
 
@@ -19,9 +47,13 @@ const TodoInputs = ({ addTodo }) => {
         type="text"
         placeholder="Input Your Task Please"
         value={input}
+        ref={inputRef}
         onChange={handleChange}
+        onKeyDown={handleKey}
       />
-      <button type="submit">Add Task</button>
+      <button type="submit">
+        {edit.id !== null ? "Update Task" : "Add Task"}
+      </button>
     </form>
   );
 };
--- src/components/TodoLists.jsx
+++ src/components/TodoLists.jsx
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
 import TodoInputs from "./TodoInputs";
 import { useState } from "react";
+import { MdDeleteForever } from "react-icons/md";
+import { FiEdit } from "react-icons/fi";
 
 const TodoLists = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+
+  const [edit, setEdit] = useState({ id: null, text: "" });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const addTodo = (task) => {
-    const newTodos = [task, ...todos];
-
-    setTodos(newTodos);
+    setTodos((prevTodo) => [...prevTodo, task]);
     console.log("Todo:", todos);
+  };
+
+  const editTodo = (todo) => {
+    setEdit({ id: todo.id, text: todo.text });
+  };
+
+  const updateTodo = (todoId, newtext) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === todoId ? { ...todo, text: newtext } : todo
+      )
+    );
+    setEdit({ id: null, text: "" });
+  };
+
+  const removeTodo = (taskToRemove) => {
+    setTodos(todos.filter((todo) => todo.id !== taskToRemove));
   };
 
   return (
     <div>
       Things To Do!
-      <TodoInputs addTodo={addTodo} />
-      {todos.map((todo, index) => (
-        <h4 key={index}>{todo}</h4>
-      ))}
+      <TodoInputs addTodo={addTodo} edit={edit} updateTodo={updateTodo} />
+      <ul>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <h4>{todo.text}</h4>
+            <FiEdit onClick={() => editTodo(todo)} />
+            <MdDeleteForever onClick={() => removeTodo(todo.id)} />
+          </div>
+        ))}
+      </ul>
     </div>
   );
 };
